c64da9b3855981a294cdc15c1b9c080096f4d740
Horieryuhei0421 <s1f101903478@iniad.org>
2021-11-15 18:04:44 +0900

    portfolio詳細

--- pages/pages/projects.js
+++ pages/pages/projects.js
@@ -61,7 +61,7 @@ export default function Projects() {
                 </p>
               </div> */}
               <div>
-                <h2>見てもらいたい点</h2>
+                <h2>見ていただきたい点</h2>
                 <p>・サービスのコンセプトを考えるところからのオリジナルサービスであり、デザインを含むフロントサイドからバックサイドまでを含んだ全体的な機能を１ヶ月ほどで大方実装したスピード感。
                 </p>
               </div>
@@ -106,7 +106,7 @@ export default function Projects() {
                 <p>・1人</p>
               </div>
               <div>
-                <h2>評価したい点</h2>
+                <h2>見ていただきたい点</h2>
                 <p>・オンラインショップでユーザーのUXの向上のために出品商品を「男性・女性」などの条件別の商品表示を実装。商品の詳細にてサイズ別に商品を選ぶことができるUXの柔軟性。
                 </p>
               </div>
@@ -123,29 +123,23 @@ export default function Projects() {
               <video src="/video/promoVideo.mp4" controls className={styles.videoFrame}></video>
               <Link href="https://horie-portfolio.vercel.app/">
                 <a target="_blank">
-                  <p className={styles.forApply}>こちらからアプリへ移動できます。</p>
+                  <p className={styles.forApply}>今ご覧いただいてるサイトになります。</p>
                 </a>
               </Link>
             </div>
             <div className={styles.projectDiscription}>
               <div>
                 <h2>サービス名</h2>
-                <p>・Promo(プロモ)</p>
+                <p>・堀江龍平のポートフォリオサイト</p>
               </div>
               <div>
                 <h2>概要</h2>
-                <p>・Webアプリ</p>
-                <p>・企業に関する知識を募ることができるアプリケーション。今後の展開としては資金を募る機能を追加し、知識と資金を募ることができ、起業を促進する起業のためのプラットフォームアプリへの移行を検討。</p>
-              </div>
-              <div>
-                <h2>機能</h2>
-                <p>・Sign up, Sign in, Sign out, Password Reset,
-                  プロフィール設定, 案件投稿, 案件削除, 購入時のトランザクシ
-                  ョン処理,（気になる企業のチェックリスト、通知機能を準備中）</p>
+                <p>・Webサイト</p>
+                <p>・堀江龍平の個人情報・過去の制作物が見やすく掲示されているポートフォリオサイトになります。Githubにも載っていない実務経験の情報なども載っています。</p>
               </div>
               <div>
                 <h2>制作期間</h2>
-                <p>・約１ヶ月＋現在進行中（追加機能制作）</p>
+                <p>・約１週間</p>
               </div>
               <div>
                 <h2>制作人数</h2>
@@ -158,14 +152,60 @@ export default function Projects() {
                 </p>
               </div> */}
               <div>
-                <h2>見てもらいたい点</h2>
-                <p>・サービスのコンセプトを考えるところからのオリジナルサービスであり、デザインを含むフロントサイドからバックサイドまでを含んだ全体的な機能を１ヶ月ほどで大方実装したスピード感。
+                <h2>見ていただきたい点</h2>
+                <p>・デザインと必要事項を最小限に絞った為見やすい構成になっている点。Next.js・TypaScriptのモダンな技術を積極的に取り入れていく姿勢。（TypeScriptへの書き換えは進行中です。）
                 </p>
               </div>
               <div>
                 <h2>使用技術
                 </h2>
-                <p>・JavaScript, React.js, Redux, Material-UI, Node.js html, css, Firebase, Git, GitHub
+                <p>・TypeScript, JavaScript, React.js, Next.js, Node.js, html, css, Versel, Git, GitHub
+                </p>
+              </div>
+            </div>
+          </section>
+          <section className={styles.projectFrame}>
+            <div className={styles.projectFrameSet}>
+              <video src="/video/promoVideo.mp4" controls className={styles.videoFrame}></video>
+              <Link href="https://horie-portfolio.vercel.app/">
+                <a target="_blank">
+                  <p className={styles.forApply}>今ご覧いただいてるサイトになります。</p>
+                </a>
+              </Link>
+            </div>
+            <div className={styles.projectDiscription}>
+              <div>
+                <h2>サービス名</h2>
+                <p>・堀江龍平のポートフォリオサイト</p>
+              </div>
+              <div>
+                <h2>概要</h2>
+                <p>・Webサイト</p>
+                <p>・堀江龍平の個人情報・過去の制作物が見やすく掲示されているポートフォリオサイトになります。Githubにも載っていない実務経験の情報なども載っています。</p>
+              </div>
+              <div>
+                <h2>制作期間</h2>
+                <p>・約１週間</p>
+              </div>
+              <div>
+                <h2>制作人数</h2>
+                <p>・1人</p>
+              </div>
+              {/* <div>
+                <h2>担当</h2>
+                <p>・全デザイン、オンラインフリーマーケット全担当。
+                  （クラウドファンディングの実装はもう一人の担当）
+                </p>
+              </div> */}
+              <div>
+                <h2>見ていただきたい点</h2>
+                <p>・デザインと必要事項を最小限に絞った為見やすい構成になっている点。Next.js・TypaScriptのモダンな技術を積極的に取り入れていく姿勢。（TypeScriptへの書き換えは進行中です。）
+                </p>
+              </div>
+              <div>
+                <h2>使用技術
+                </h2>
+                <p>・TypeScript, JavaScript, React.js, Next.js, Node.js, html, css, Versel, Git, GitHub
                 </p>
               </div>
             </div>
